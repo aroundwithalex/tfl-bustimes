65265d646744dda7745e5c3cd2715f9c12a14923
Alex Pope <67066474+aroundwithalex@users.noreply.github.com>
2021-01-28 17:52:34 +0000

    Refactor test_fetch.py
    Change test methods in accordance to changes to fetch.py. Check for list
    instead of generator in test_fetch and check for returned item in
    test_dict method

--- tests/test_fetch.py
+++ tests/test_fetch.py
@@ -1,19 +1,16 @@
 import unittest
-import types
 from app.fetch import fetch_data, data_for_display
 
 
 class TestFetchMethods(unittest.TestCase):
     def test_fetch(self):
         res = fetch_data()
-        self.assertIsInstance(res, types.GeneratorType)
+        self.assertIsInstance(res, list)
 
     def test_dict(self):
         res = fetch_data()
-        for item in res:
-            data = data_for_display(item)
-            print(data)
-            self.assertEqual(len(data.keys()), 8)
+        data = data_for_display(res)
+        self.assertIsNotNone(data)
 
 
 if __name__ == "__main__":
