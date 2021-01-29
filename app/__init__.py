4391eada47dcd543368ee9f3fb5393c743f550a9
Alex Pope <67066474+aroundwithalex@users.noreply.github.com>
2021-01-29 09:33:58 +0000

    Add Config reader to init.py
    Call Confing from config.py, in order to set basic Flask configuration
    variables.

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,9 @@
 from flask import Flask
+from config import Config
 
-app = Flask(__name__)
-app.config["JSONIFY_PRETTYPRINT_REGULAR"] = True
+
+app = Flask(__name__, instance_relative_config=True)
+app.config.from_object(Config)
+
 
 from app import routes
