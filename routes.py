52b6bf6d133d9f8e512df268e0981545684fdbac
Alex Pope <67066474+aroundwithalex@users.noreply.github.com>
2021-01-29 15:43:56 +0000

    Refactor project structure
    Remove the apps folder and place Flask project files in main directory.
    This was done in order to simplify project structure, in accordance to
    the Zen of Python 'flat is better than nested

--- routes.py
+++ routes.py
@@ -1,5 +1,6 @@
 from flask import render_template, jsonify
-from .fetch import fetch_data, data_for_display
+from fetch import fetch_data, data_for_display
+from db import write_data, read_data
 from app import app
 
 
@@ -7,6 +8,7 @@ from app import app
 def index():
     data = fetch_data()
     display_data = data_for_display(data)
+    write_data(display_data)
     return render_template("index.html", data=display_data)
 
 
@@ -14,3 +16,9 @@ def index():
 def raw_data():
     data = fetch_data()
     return jsonify(data)
+
+
+@app.route("/history.html")
+def history():
+    data = read_data()
+    return render_template("history.html", data=data)
