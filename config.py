c6badec8f1f6e8fd4bfef9e673256f2e88eb3aa6
Alex Pope <67066474+aroundwithalex@users.noreply.github.com>
2021-01-30 13:16:09 +0000

    Docstrings added to classes

--- config.py
+++ config.py
@@ -4,28 +4,41 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 
 
 class Config(object):
+    """
+    Sets Configuration variables for Flask app
+
+    Configuration variables are set that are designed to be used in production.  SECRET_KEY set for security purposes, though there are no user forms.  Likewise for CSRF capabilities.
+    """
     DEBUG = False
     TESTING = False
     CSRF_ENABLED = True
     SECRET_KEY = os.urandom(25)
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
-    SQLALCHEMY_DATABASE_URL = os.environ["DATABASE_URL"]
     JSONIFY_PRETTYPRINT_REGULAR = True
 
-
-class ProductionConfig(Config):
-    DEBUG = False
-
-
 class StagingConfig(Config):
+    """
+    Sets Staging Configuration.  
+    
+    Sets up server in development mode and sets debug variable to true.  Inherits from Config file.
+    """
     DEVELOPMENT = True
     DEBUG = True
 
 
 class DevelopmentConfig(Config):
+    """
+    Sets Development Config.
+
+    Sets up testing server and sets debug variable to true.  Inherits from Config file.
+    """
     TESTING = True
     DEBUG = True
 
 
 class TestingConfig(Config):
+    """
+    Sets Testing Config.
+
+    Sets up testing server.  Inherits from Config file.
+    """
     TESTING = True
